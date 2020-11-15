d354c217c45032edebda85feeb5b2da6ec2339bd
小白 <1140279968@qq.com>
2020-11-15 12:40:00 +0800

    feat: 🎸 使用高阶函数进行重构
    使用高阶函数进行重构

--- src/components/breadcrumb/index.tsx
+++ src/components/breadcrumb/index.tsx
@@ -14,6 +14,29 @@ const BreadcrumbSeparator: React.FC = ({ children, ...restProps }) => (
 		{children}
 	</li>
 )
+
+const withSeparator = (lastIndex: any, separator: React.ReactNode) => (cur: any, pre: any, index: any) => {
+	const notLast = index < lastIndex
+	if (notLast) {
+		cur.push(pre, <BreadcrumbSeparator key={`breadcrumb_sep${index}`}>{separator}</BreadcrumbSeparator>)
+	} else {
+		cur.push(pre)
+	}
+	return cur
+}
+const withCollapse = ({
+	itemsBefore, itemsAfter, max, _children, totalItems, open
+}: {
+	itemsBefore: number
+	itemsAfter: number
+	max: number
+	_children: any
+	totalItems: number
+	open: ()=> void
+}) => [
+	..._children.slice(0, itemsBefore), <Collapser title='Expand' key='collapsed-seperator' onClick={open} />,
+	..._children.slice(totalItems - itemsAfter, totalItems)
+]
 const BreadcrumbItem: React.FC = ({ children }) => (
 	<li className="breadcrumb-item">
 		{children}
@@ -24,35 +47,31 @@ const Breadcrumb: React.FC<BreadcrumbProps> = ({ separator = '/', children, coll
 	const { expanded, open } = useBreadcrumb()
 
 	// 对children进行处理
+	// 判断是否是最后一个来决定是否添加分隔符
 	let _children = React.Children.toArray(children)
+	const lastIndex = _children.length - 1
 	_children = _children.map((child, index) => (
 		<BreadcrumbItem key={`breadcrumb_item${index}`}>{child}</BreadcrumbItem>
 	))
+		.reduce(withSeparator(lastIndex, separator), [])
 
-	// 判断是否是最后一个来决定是否添加分隔符
-	const lastIndex = _children.length - 1
-	_children = (_children.reduce((pre, cur, index) => {
-		const notLast = index < lastIndex
-		if (notLast) {
-			(pre as React.ReactNodeArray).push(cur, <BreadcrumbSeparator key={`breadcrumb_sep${index}`}>{separator}</BreadcrumbSeparator>)
-		} else {
-			(pre as React.ReactNodeArray).push(cur)
-		}
-		return pre
-	}, [])) as any
+
+	
 
 
 	// 通过collapse属性来判断是否是可展开的
 	const totalItems = _children.length
 
 	if (!expanded || totalItems <= max) {
-		_children = [
-			..._children.slice(0, itemsBefore),
-			<Collapser title="Expand" onClick={open} key='collapsed-seperator'></Collapser>,
-			..._children.slice(totalItems - itemsAfter, totalItems)
-		]
+		_children = withCollapse({
+			itemsBefore,
+			itemsAfter,
+			totalItems,
+			max,
+			_children,
+			open
+		})
 	}
-	console.log(expanded, _children)
 	return (
 		<ol {...restProps}>
 			{_children}
--- src/components/useBreadcrumb.ts
+++ src/components/useBreadcrumb.ts
@@ -3,7 +3,6 @@ import { useState } from 'react'
 export const useBreadcrumb  = () => {
 	const [ expanded, setExpanded ] = useState(false)
 	const open = () => {
-		console.log(1)
 		setExpanded(true)
 	}
     
